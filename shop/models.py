3265fbc29622602f79a1733b825ab65e61657755
Arnat Asanov <asanovarnat2209@gmail.com>
2025-07-01 00:19:43 +0600

    add&update templates

--- shop/models.py
+++ shop/models.py
@@ -8,12 +8,12 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=300)
     description = models.TextField(blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.URLField()
+    image = models.CharField(max_length=1000, blank=True)
 
     def __str__(self):
         return self.name
