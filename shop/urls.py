60ad1f4c081a5916ed0abc0ca14d01c1e87a52d5
Arnat Asanov <asanovarnat2209@gmail.com>
2025-07-01 00:58:57 +0600

    update templates, added delete/edit urls&views

--- shop/urls.py
+++ shop/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('products/', views.products_view, name='products'),
     path('products/<int:pk>/', views.product_view, name='product_detail'),
     path('products/add/', views.product_add_view, name='product_add'),
+    path('products/<int:pk>/edit/', views.product_edit_view, name='product_edit'),
+    path('products/<int:pk>/delete/', views.product_delete_view, name='product_delete'),  # добавлен путь удаления
     path('categories/add/', views.category_add_view, name='category_add'),
+    path('categories/<int:pk>/', views.category_detail_view, name='category_detail'),
+    path('categories/<int:pk>/delete/', views.category_delete_view, name='category_delete'),
 ]
+
--- shop/views.py
+++ shop/views.py
@@ -2,30 +2,62 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product, Category
 from .forms import CategoryForm, ProductForm
 
+
 def products_view(request):
     products = Product.objects.all()
     return render(request, 'shop/products_list.html', {'products': products})
 
-def product_view(request, id):
-    product = get_object_or_404(Product, id=id)
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
     return render(request, 'shop/product_detail.html', {'product': product})
-
-def category_add_view(request):
-    if request.method == 'POST':
-        form = CategoryForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('categories_add')
-    else:
-        form = CategoryForm()
-    return render(request, 'shop/category_add.html', {'form': form})
 
 def product_add_view(request):
     if request.method == 'POST':
         form = ProductForm(request.POST)
         if form.is_valid():
             product = form.save()
-            return redirect('product_detail', id=product.id)
+            return redirect('product_detail', pk=product.pk)
     else:
         form = ProductForm()
     return render(request, 'shop/product_add.html', {'form': form})
+
+def product_edit_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_detail', pk=product.pk)
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'shop/product_edit.html', {'form': form, 'product': product})
+
+def product_delete_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('products')
+    return render(request, 'shop/product_confirm_delete.html', {'product': product})
+
+#
+
+def category_add_view(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('category_add')
+    else:
+        form = CategoryForm()
+    return render(request, 'shop/category_add.html', {'form': form})
+
+def category_detail_view(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    return render(request, 'shop/category_detail.html', {'category': category})
+
+def category_delete_view(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('categories_list')
+    return render(request, 'shop/category_confirm_delete.html', {'category': category})
