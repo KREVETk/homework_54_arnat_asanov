7dbcd66ddc4d467dde83c7b7b722c2cde6c607ea
Arnat Asanov <asanovarnat2209@gmail.com>
2025-06-30 22:24:39 +0600

    Initial setup settings, urls, viewa

--- shop/views.py
+++ shop/views.py
@@ -4,23 +4,23 @@ from .forms import ProductForm, CategoryForm
 
 def products_view(request):
     products = Product.objects.all()
-    return render(request, 'store/products.html', {'products': products})
+    return render(request, 'shop/products.html', {'products': products})
 
 def product_view(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    return render(request, 'store/product_detail.html', {'product': product})
+    return render(request, 'shop/product_detail.html', {'product': product})
 
 def product_add_view(request):
     form = ProductForm(request.POST or None)
     if form.is_valid():
         product = form.save()
         return redirect('product_detail', pk=product.pk)
-    return render(request, 'store/product_form.html', {'form': form})
+    return render(request, 'shop/product_form.html', {'form': form})
 
 def category_add_view(request):
     form = CategoryForm(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect('products')
-    return render(request, 'store/category_form.html', {'form': form})
+    return render(request, 'shop/category_form.html', {'form': form})
 
