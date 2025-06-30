de74e78c933c211eb787eb88f9aa51d006e9dd56
Arnat Asanov <asanovarnat2209@gmail.com>
2025-06-30 22:34:58 +0600

    makemigrations, page htmls no functional yet

--- shop/models.py
+++ shop/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
 
-
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.URLField()
--- shop/views.py
+++ shop/views.py
@@ -1,26 +1,31 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Product
-from .forms import ProductForm, CategoryForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product, Category
+from .forms import CategoryForm, ProductForm
 
 def products_view(request):
     products = Product.objects.all()
-    return render(request, 'shop/products.html', {'products': products})
+    return render(request, 'shop/products_list.html', {'products': products})
 
-def product_view(request, pk):
-    product = get_object_or_404(Product, pk=pk)
+def product_view(request, id):
+    product = get_object_or_404(Product, id=id)
     return render(request, 'shop/product_detail.html', {'product': product})
 
-def product_add_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        product = form.save()
-        return redirect('product_detail', pk=product.pk)
-    return render(request, 'shop/product_form.html', {'form': form})
-
 def category_add_view(request):
-    form = CategoryForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('products')
-    return render(request, 'shop/category_form.html', {'form': form})
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories_add')
+    else:
+        form = CategoryForm()
+    return render(request, 'shop/category_add.html', {'form': form})
 
+def product_add_view(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save()
+            return redirect('product_detail', id=product.id)
+    else:
+        form = ProductForm()
+    return render(request, 'shop/product_add.html', {'form': form})
