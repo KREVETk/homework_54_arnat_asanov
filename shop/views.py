13363d2ba58ae464f124ad0bedd87076eae55e4b
Arnat Asanov <asanovarnat2209@gmail.com>
2025-06-30 22:14:07 +0600

    Init commit

---
+++ shop/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+from .forms import ProductForm, CategoryForm
+
+def products_view(request):
+    products = Product.objects.all()
+    return render(request, 'store/products.html', {'products': products})
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'store/product_detail.html', {'product': product})
+
+def product_add_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        product = form.save()
+        return redirect('product_detail', pk=product.pk)
+    return render(request, 'store/product_form.html', {'form': form})
+
+def category_add_view(request):
+    form = CategoryForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('products')
+    return render(request, 'store/category_form.html', {'form': form})
+
